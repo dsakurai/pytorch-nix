16518e48f426676a47702aa8a502f68686acd48b
Daisuke Sakurai <d.sakurai0@gmail.com>
2025-05-17 16:38:32 +0000

    pytorch can use CUDA on WSL2

--- flake.nix
+++ flake.nix
@@ -6,24 +6,47 @@
 
   outputs = { self, nixpkgs }: {
     defaultPackage.x86_64-linux = let
-      pkgs = import nixpkgs { system = "x86_64-linux"; };
+      pkgs = import nixpkgs {
+         system = "x86_64-linux";
+         config = {
+           allowUnfree = true;
+           cudaSupport = true;
+         };
+      };
     in pkgs.mkShell {
       buildInputs = with pkgs; [
-        python313
+        python312
+        python312Packages.pytorch-bin
+        # python312Packages.pytorchWithCuda
         poetry # <- Uses Nix's system default python by default
         pkgs.gcc.cc.lib # for jupyter
       ];
 
       shellHook = ''
           # Pass the libraries (to poetry's virtual environment for using Python packages)
+          export LD_LIBRARY_PATH="" # Clear system lib path, just for safety.
+          
+          # For poetry
           export "LD_LIBRARY_PATH=${pkgs.gcc.cc.lib}/lib:$LD_LIBRARY_PATH"
+
+          export LD_LIBRARY_PATH="${pkgs.cudaPackages.cudatoolkit.lib}/lib:${pkgs.cudaPackages.libcublas.lib}/lib:$LD_LIBRARY_PATH"
+          export LD_LIBRARY_PATH="/usr/lib/wsl/lib:$LD_LIBRARY_PATH"
+
+          # Find libcuda for WSL
+          CUDA_PATH=$(find /usr/lib/wsl/drivers -name 'libcuda.so.*' | head -n1)
+          if [ -n "$CUDA_PATH" ]; then
+            export LD_LIBRARY_PATH="$(dirname "$CUDA_PATH"):$LD_LIBRARY_PATH"
+            echo "[INFO] Found libcuda at $CUDA_PATH"
+          else
+            echo "[WARN] libcuda.so not found"
+          fi
 
           poetry config virtualenvs.in-project true
           poetry init --no-interaction
 
-          poetry env use python3.13
+          poetry env use python3.12
           # Hard-code the Python version in the poetry project setting.
-          sed -i 's/requires-python = ">=3.12"/requires-python = "^3.13"/' pyproject.toml
+          sed -i 's/requires-python = ">=3.12"/requires-python = "^3.12"/' pyproject.toml
 
           # You may add Python packages with poetry here, but it will be saved in the TOML file anyway.
           # poetry add jupyterlab notebook ipykernel
@@ -31,7 +54,7 @@
           poetry install --no-root # Install Python packages
 
           # Activate environment
-          eval $(poetry env activate)
+          # eval $(poetry env activate)
         '';
 
     };
