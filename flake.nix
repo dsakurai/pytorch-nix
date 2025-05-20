f1077d2febcd693bd1a17ac7dd0abbc9c4908da5
Daisuke Sakurai <d.sakurai0@gmail.com>
2025-05-20 13:50:25 +0000

    Minimal flake.nix config

--- flake.nix
+++ flake.nix
@@ -18,20 +18,17 @@
       buildInputs = with pkgs; [
         python312
         # python312Packages.pytorch-bin # for pytorch from nixpkgs, tested with WSL2 (with CUDA) and podman, where the podman machine has NVIDIA's nvidia's container toolkit installed.
-        # cudaPackages_12_6.cuda_nvcc # You can check the CUDA version with `nvcc --version`, for example.
         
         # Use CUDA like below in shellHook:
         # export LD_LIBRARY_PATH="${pkgs.cudaPackages_12_6.cudatoolkit.lib}/lib:${pkgs.cudaPackages_12_6.libcublas.lib}/lib:$LD_LIBRARY_PATH"
+        # cudaPackages_12_6.cuda_nvcc # You can check the CUDA version with `nvcc --version`, for example.
 
         poetry # <- Uses Nix's system default python by default
       ];
 
       shellHook = ''
-          # # Pass the libraries (to poetry's virtual environment for using Python packages)
-          # export LD_LIBRARY_PATH="" # Clear system lib path, just for safety.
-          # 
-          # For poetry
-          export "LD_LIBRARY_PATH=${pkgs.gcc.cc.lib}/lib:$LD_LIBRARY_PATH"
+          # Pass the libraries (to poetry's virtual environment for using Python packages)
+          export "LD_LIBRARY_PATH=${pkgs.gcc.cc.lib}/lib:$LD_LIBRARY_PATH" # Used by torch
 
 
           # Find libcuda for WSL
