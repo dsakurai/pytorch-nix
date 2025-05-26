7f50035b00eeb6a3ccf0df43e96752be5a8a19ce
Daisuke Sakurai <d.sakurai0@gmail.com>
2025-05-26 14:13:38 +0000

    Restructure files

--- source/jupyter-lab-nix.sh
+++ source/jupyter-lab-nix.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env /bin/bash
 set -e
 
-# Note: this has to internally start the jupyter server 
+# the directory of this script
+SCRIPT_DIR="$(dirname "${BASH_SOURCE[0]}")"
+
+# Change directory to this script's
+cd "$SCRIPT_DIR"
+
 nix develop --extra-experimental-features nix-command --extra-experimental-features flakes --command bash -c 'eval $(poetry env activate) && jupyter lab --no-browser'
