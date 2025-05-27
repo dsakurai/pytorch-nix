593c366b6825a2876e158d784185a3a8a7e3a4ba
Daisuke Sakurai <d.sakurai0@gmail.com>
2025-05-27 12:44:54 +0000

    Move script jupyter-lab-nix.sh

--- project/jupyter-lab-nix.sh
+++ project/jupyter-lab-nix.sh
@@ -9,6 +9,6 @@ set -e
 SCRIPT_DIR="$(dirname "${BASH_SOURCE[0]}")"
 
 # Change directory to this script's
-cd "$SCRIPT_DIR/project"
+cd "$SCRIPT_DIR"
 
 nix develop --extra-experimental-features nix-command --extra-experimental-features flakes ../nix-scripts --command bash -c 'eval $(poetry env activate) && export JUPYTER_CONFIG_DIR="$(pwd)/.jupyter" && jupyter lab --no-browser'
